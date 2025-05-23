217eb7dbfe7dbca60dbbffe8ea45604140dc62e0
Ancient <arthas.you@163.com>
2025-05-23 15:08:53 +0800

    feat:adding routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router";
+import router from "./routes/routes.tsx";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Root element #root not found");
 
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
