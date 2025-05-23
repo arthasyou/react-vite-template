c2bf4f4c26fdb843ac753853db376cd5d7598135
Ancient <arthas.you@163.com>
2025-05-23 18:04:38 +0800

    feat: adding provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
+import { Provider } from "react-redux";
+import { store } from "./store";
 import router from "./routes";
 import "./index.css";
 import "./i18n";
@@ -10,6 +12,8 @@ if (!rootElement) throw new Error("Root element #root not found");
 
 createRoot(rootElement).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
--- src/pages/protected/Home.tsx
+++ src/pages/protected/Home.tsx
@@ -1,7 +1,12 @@
 import { useTranslation } from "react-i18next";
+import { useSelector, useDispatch } from "react-redux";
+import type { RootState } from "@/store";
+import { increment, decrement } from "@/store/counterSlice";
 
 export default function Home() {
   const { t, i18n } = useTranslation();
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <div className="p-6">
@@ -22,6 +27,17 @@ export default function Home() {
         >
           English
         </button>
+      </div>
+      <div className="p-6">
+        <h1 className="text-xl font-bold">Count: {count}</h1>
+        <div className="space-x-4 mt-4">
+          <button type="button" onClick={() => dispatch(increment())}>
+            +
+          </button>
+          <button type="button" onClick={() => dispatch(decrement())}>
+            -
+          </button>
+        </div>
       </div>
     </div>
   );
